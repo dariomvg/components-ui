371d08c6c3e696234af61184fb64a24d54d98322
dariomvg <contact.dario.info@gmail.com>
2024-03-25 11:37:49 -0300

    second commit

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,8 +1,8 @@
 import "../styles/Toggle.css";
 
-const Toggle = () => {
+const Toggle = ({onClick}) => {
   return (
-    <label className="switch">
+    <label className="switch" onClick={onClick}>
   <input type="checkbox" />
   <span className="slider"></span>
 </label>
